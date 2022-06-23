a8f80b3323818cded2e2b83bfb7884606bc890f8
Sivaram Balakrishnan <sivaram1992@gmail.com>
2022-06-23 21:47:16 +0530

    use createRoot

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import { App } from './Components/App.js'
 
-const { createElement } = window.React
-const { render } = window.ReactDOM
-
-render(<App />, document.getElementById('root'))
+const { createRoot } = window.ReactDOM
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />)
